7c8c013be16c093f75fb76ed24bbe9fd251111b3
Alva Karlsson <alva_karlsson@live.se>
2022-11-10 12:11:44 +0000

    Added the questions to the js file

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,29 @@
-
+const theQuestions = [
+    {
+        question: "Between which years was Kalmar the Capital city of Scandinavia?",
+        answers: {
+            a: "1435 - 1546",
+            b: "1397 - 1523",
+            c: "1362 - 1489"
+        },
+        correctAnswer: "b"
+    },
+    {
+        question: "Kalmar is famous for its' castle, when did the construction of it started?",
+        answers: {
+            a: "The end of the 12th century",
+            b: "The middle of the 12th century",
+            c: "The beginning of the 13th century"
+        },
+        correctAnswer: "a"
+    },
+    {
+        question: "In Kalmar, a very large sporting event is held every year, which one?",
+        answers: {
+            a: "Ironman",
+            b: "Diamond League",
+            c: "Vasaloppet"
+        },
+        correctAnswer: "a"
+    }
+];
